8ed0759860a07f77046cf5ab731111a3d62ef1ca
BeatrizCortes <34442980+BeatrizCortes@users.noreply.github.com>
2019-04-29 12:15:04 +0200

    Update 05_GWAS.R

--- 05_GWAS/05_GWAS.R
+++ 05_GWAS/05_GWAS.R
@@ -1,5 +1,5 @@
 ######################################################################################
-# 05 GWAS                                                                            #
+# 05 GWAS Preparation                                                                #
 ######################################################################################
 
 # Libraries
@@ -103,6 +103,17 @@ colnames(freq_p)[5:6] = c("ALT_FREQS_PANDORA","OBS_CT_PANDORA")
 
 # Now we can make the merge of the data 
 freqs = left_join(freq_g,freq_p)
+
+head(freqs)
+colnames(freqs) = c("chr", 
+                    "ID", 
+                    "Ref_freqs", 
+                    "Alt_freqs",
+                    "ALT_FREQS_GCAT", 
+                    "N_GCAT", 
+                    "ALT_FREQS_PANDORA", 
+                    "N_Pandora")
+head(freqs)
 
 # Now we calculate the difference for each frequency between Pandora and GCAT
 freqs$diff_Pandora_GCAT = abs(freqs$ALT_FREQS_GCAT-freqs$ALT_FREQS_PANDORA)
@@ -301,3 +312,263 @@ differences[which(!is.na(differences$provean_pred)),] %>% select(chr_pos,
 
 # All the other variants have not classification:
 nrow(differences) -3 # 448
+
+# We want to see the N of this variants with high differences
+
+differences2 = freqs %>% filter(diff_Pandora_GCAT > 0.2) %>% select(ID,
+                                                                    REF,
+                                                                    ALT,
+                                                                    ALT_FREQS_GCAT,
+                                                                    OBS_CT_GCAT,
+                                                                    Ref,
+                                                                    Alt, 
+                                                                    ALT_FREQS_PANDORA,
+                                                                    OBS_CT_PANDORA,
+                                                                    diff_Pandora_GCAT)
+
+dim(differences2)
+
+# Make the table
+table(differences2$OBS_CT_PANDORA)
+
+# Look the ones with < 100 observations
+
+differences_smaller100 = differences2 %>% filter(OBS_CT_PANDORA > 100)
+dim(differences_smaller100) # 88 variants observed in less than 100 individuals  
+head(differences_smaller100)
+
+# Rare variants with high observation rate
+differences_high = differences2 %>% filter(OBS_CT_PANDORA > (90*3162/100))
+dim(differences_high) 
+head(differences_high)
+table(differences_high$OBS_CT_PANDORA)
+hist(differences_high$diff_Pandora_GCAT)
+
+# Rare variants with high observation rate
+differences_high2 = differences2 %>% filter(OBS_CT_PANDORA >= (3162))
+dim(differences_high2) 
+head(differences_high2)
+table(differences_high2$OBS_CT_PANDORA)
+hist(differences_high2$diff_Pandora_GCAT)
+
+
+# So we filter the data to extract the variants with this difference < 0.2
+
+# In the bim file we have all the SNPs we have previously selected and we have to delete the ones with difference > 0.2
+head(bim) # We have chr_pos
+dim(bim) # 13718 variants
+
+# In differences we have all the ones with diff_Pandora_GCAT > 0.2
+head(differences) # and we have also chr_pos 
+dim(differences) # 451 variants
+
+head(differences_smaller100) # IVAN
+
+### Filter the freqs by the N_pandora
+
+# 
+dim(freqs) # 13861    15
+first_final_subset = freqs %>% filter(N_Pandora > (90*3162/100))
+dim(first_final_subset) # 13530    15
+# With this filter we loose 331 variants
+
+png("graphs/differences_PG_filtered.png")
+hist(first_final_subset$diff_Pandora_GCAT,
+     main = "Differences frequencies Pandora vs GCAT",
+     xlab = "Differences") 
+dev.off()
+
+png("graphs/differences_PG_filtered.png")
+hist((first_final_subset %>% filter(diff_Pandora_GCAT > 0.2))$diff_Pandora_GCAT,
+     main = "Differences frequencies Pandora vs GCAT (>0.2)",
+     xlab = "Differences")
+dev.off()
+
+
+
+#22_21344019 prove that pandora_nuestro is ok
+
+selected_samples = fread("/imppc/labs/dnalab/share/PRS/02_Pandora_data_preparation/selected_merge_samples.txt")
+
+dim(selected_samples)
+
+aux = selected_samples %>% filter(chr %in% 22 & start %in% 21344019)
+
+# Clasify gaps search for each gap if it is because they do not search the variable or not found it
+clasify_gaps <- function(dataset){
+  for(i in 7:(ncol(dataset)-1)){ # 1:1588 Sample by sample # The last column is the chr_pos
+    # Take the id of the sample
+    print(i)
+    id_sample = colnames(dataset)[i]
+    # Search the panel of this sample
+    panel_sample = samples_information[samples_information$sampleId %in% 
+                                         id_sample,]$version_panel
+    # take as sample the genotypes of this sample
+    sample = as.character(t(dataset[,i,with=F]))
+    # Search for the empty genotypes on the sample: we do not know if they are NA or 0/0
+    which_vacio = which(sample=="")
+    # Search the empty variants 
+    homos = panel[which(panel$chr_pos %in% dataset$chr_pos[which_vacio]),]
+    # Search the ones with true in the correspondent panel which means this variant have been analyzed (it is 0/0)
+    which_homo = as.logical(t(homos[,which(colnames(homos) %in% panel_sample),with=F]))==TRUE
+    # Take the chr_pos of this 0/0 variants 
+    homo_positions = homos$chr_pos[which(which_homo==T)]
+    # Change this chr_pos to the 0/0
+    dataset[dataset$chr_pos %in% homo_positions,i] = "0/0" 
+  }
+  return(dataset)
+}
+
+panel = fread("/imppc/labs/dnalab/share/PRS/04_Merge_datasets/output_data/panels_subset.txt")
+aux2 = clasify_gaps(selected_samples)
+
+aux3 = aux2 %>% filter(chr %in% 22 & start %in% 21344019)
+table(as.character(aux3[,7:1587]))
+
+
+interested_snps = fread("/imppc/labs/dnalab/share/PRS/03_GCAT_subset_variants_imputed/intersection_snps.txt")
+
+head(interested_snps)
+
+interested_snps[interested_snps$chr_pos %in% "22_21344019",]
+
+
+head(first_final_subset)
+
+# Calculate the CI 
+
+# GCAT
+first_final_subset$lowerGCAT = first_final_subset$ALT_FREQS_GCAT - 1.96 * sqrt((first_final_subset$ALT_FREQS_GCAT*(1-first_final_subset$ALT_FREQS_GCAT))/first_final_subset$N_GCAT)
+first_final_subset$uperGCAT = first_final_subset$ALT_FREQS_GCAT + 1.96 * sqrt((first_final_subset$ALT_FREQS_GCAT*(1-first_final_subset$ALT_FREQS_GCAT))/first_final_subset$N_GCAT)
+
+# Pandora
+first_final_subset$lowerPandora = first_final_subset$ALT_FREQS_PANDORA - 1.96 * sqrt((first_final_subset$ALT_FREQS_PANDORA*(1-first_final_subset$ALT_FREQS_PANDORA))/first_final_subset$N_Pandora)
+first_final_subset$uperPandora = first_final_subset$ALT_FREQS_PANDORA + 1.96 * sqrt((first_final_subset$ALT_FREQS_PANDORA*(1-first_final_subset$ALT_FREQS_PANDORA))/first_final_subset$N_Pandora)
+
+?findInterval
+
+x <- c(4,4.5)
+v <- c(5, 10) # create two bins [5,10) and [10,15)
+findInterval(x, v)
+
+first_final_subset$findInterval = FALSE
+
+for(i in 1:nrow(first_final_subset)){
+  
+  x = c(first_final_subset$lowerGCAT[i],first_final_subset$uperGCAT[i])
+  v = c(first_final_subset$lowerPandora[i],first_final_subset$uperPandora[i])
+  
+  aux = findInterval(x, v)
+  
+  if(sum(aux %in% 1)>0){first_final_subset$findInterval[i] = TRUE}
+  
+  
+}
+
+table(first_final_subset$findInterval)
+
+prova = first_final_subset[first_final_subset$findInterval,]
+
+dim(prova)
+
+hist(prova$diff_Pandora_GCAT)
+
+prova %>% arrange(desc(diff_Pandora_GCAT)) %>% head()
+
+hist(first_final_subset[676:nrow(first_final_subset),]$ALT_FREQS_PANDORA)
+
+
+# Select the variables we want to discard which are the ones with differences between pandora and GCAT > 0.2
+discard_variants_first_subset = first_final_subset %>% filter(diff_Pandora_GCAT >0.2) %>% select(ID)
+fwrite(discard_variants_first_subset, "discard_variants_first_subset.txt")
+
+
+
+system(paste0(" /imppc/labs/dnalab/share/PRS/03_GCAT_subset_variants_imputed/plink2 ",
+              " --pgen /imppc/labs/dnalab/share/PRS/05_GWAS/prs_data.pgen ",
+              " --pvar /imppc/labs/dnalab/share/PRS/05_GWAS/prs_data.pvar ",
+              " --psam /imppc/labs/dnalab/share/PRS/05_GWAS/prs_data.psam ",
+              " --exclude discard_variants_first_subset.txt ",
+              " --geno 0.1 --make-pgen ",
+              " --out /imppc/labs/dnalab/share/PRS/05_GWAS/outputs_plink/first_final_subset"))
+
+
+
+# Now we make a PCA with a filter to the rare variants (maf > 0.01)
+system(paste0(" /imppc/labs/dnalab/share/PRS/03_GCAT_subset_variants_imputed/plink2 ",
+              " --pgen /imppc/labs/dnalab/share/PRS/05_GWAS/outputs_plink/first_final_subset.pgen ",
+              " --pvar /imppc/labs/dnalab/share/PRS/05_GWAS/outputs_plink/first_final_subset.pvar ",
+              " --psam /imppc/labs/dnalab/share/PRS/05_GWAS/outputs_plink/first_final_subset.psam ",
+              " --pca approx --maf 0.01 ",
+              " --out /imppc/labs/dnalab/share/PRS/05_GWAS/outputs_plink/first_final_subset"))
+
+# Read the Pca eigenvectors values 
+pca_out = fread("outputs_plink/first_final_subset.eigenvec")
+
+head(pca_out)
+dim(pca_out)
+
+# Prepare the colours
+my_cols = c(rep(1,4988),rep(2,1581))
+
+# Make the plot
+png("graphs/pca2.png")
+plot(pca_out$PC1,pca_out$PC2,pch=16,col=my_cols, 
+     main = "Pandora vs GCAT PCA", 
+     xlab= "PC1", ylab = "PC2")
+legend("bottomleft",legend = c("GCAT", "Pandora"), col = c("black", "red"), 
+       title = "Populations", pch = 16)
+dev.off() 
+
+
+# Select the variables we want to discard which are the ones with differences between pandora and GCAT > 0.05
+discard_variants_first_subset = first_final_subset %>% filter(diff_Pandora_GCAT >0.01) %>% select(ID)
+fwrite(discard_variants_first_subset, "discard_variants_first_subset_001.txt")
+
+
+
+system(paste0(" /imppc/labs/dnalab/share/PRS/03_GCAT_subset_variants_imputed/plink2 ",
+              " --pgen /imppc/labs/dnalab/share/PRS/05_GWAS/prs_data.pgen ",
+              " --pvar /imppc/labs/dnalab/share/PRS/05_GWAS/prs_data.pvar ",
+              " --psam /imppc/labs/dnalab/share/PRS/05_GWAS/prs_data.psam ",
+              " --exclude discard_variants_first_subset_001.txt ",
+              " --geno 0.1 --make-pgen ",
+              " --out /imppc/labs/dnalab/share/PRS/05_GWAS/outputs_plink/first_final_subset_001"))
+
+
+
+# Now we make a PCA with a filter to the rare variants (maf > 0.01)
+system(paste0(" /imppc/labs/dnalab/share/PRS/03_GCAT_subset_variants_imputed/plink2 ",
+              " --pgen /imppc/labs/dnalab/share/PRS/05_GWAS/outputs_plink/first_final_subset_001.pgen ",
+              " --pvar /imppc/labs/dnalab/share/PRS/05_GWAS/outputs_plink/first_final_subset_001.pvar ",
+              " --psam /imppc/labs/dnalab/share/PRS/05_GWAS/outputs_plink/first_final_subset_001.psam ",
+              " --pca approx --maf 0.01 ",
+              " --out /imppc/labs/dnalab/share/PRS/05_GWAS/outputs_plink/first_final_subset_001"))
+
+# Read the Pca eigenvectors values 
+pca_out = fread("outputs_plink/first_final_subset_001.eigenvec")
+
+head(pca_out)
+dim(pca_out)
+
+# Prepare the colours
+my_cols = c(rep(1,4988),rep(2,1581))
+
+# Make the plot
+png("graphs/pca2_001.png")
+plot(pca_out$PC1,pca_out$PC2,pch=16,col=my_cols, 
+     main = "Pandora vs GCAT PCA", 
+     xlab= "PC1", ylab = "PC2")
+legend("bottomleft",legend = c("GCAT", "Pandora"), col = c("black", "red"), 
+       title = "Populations", pch = 16)
+dev.off()
+
+
+## Juntar 70K SNPs de 1000G (1000Gselected.pvar) y GCAT-Pandora 3K SNPs
+
+test = fread("things/1000Gselected.pvar",skip=188)
+
+dim(test)
+head(test)
+length(unique(test$ID))
+
